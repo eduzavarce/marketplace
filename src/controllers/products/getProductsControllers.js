8795c480d2e413846f842cb60e80429eed156a4b
Isabel PM <ispemaz@gmail.com>
2023-05-14 11:02:05 +0200

    cambios pictures

--- src/controllers/products/getProductsControllers.js
+++ src/controllers/products/getProductsControllers.js
@@ -1,10 +1,27 @@
-const { findAllProducts } = require('../../repositories');
+const { createImageUrl } = require('../../helpers');
+const {
+  findAllProducts,
+  findImagesByIdProduct,
+} = require('../../repositories');
 
 const findAllProductsController = async (req, res, next) => {
   try {
     const products = await findAllProducts();
 
-  
+    const productsImages = await Promise.all(
+      products.map(async (product) => {
+        const picturesFileNames = await findImagesByIdProduct(product.id);
+
+        const pictures = await Promise.all(
+          picturesFileNames.map((picture) => {
+            console.log('filename: ', picture.fileName, product.id);
+            return createImageUrl(picture.fileName, product.id, 'products');
+          })
+        );
+      })
+    );
+    console.log(productsImages);
+
     res.status(200);
 
     res.send({
